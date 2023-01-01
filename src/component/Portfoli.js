67b54f22d7ecbee3270ed3d0ca493d2ed9b1cca2
din1998 <dinislam8803@gmail.com>
2023-01-01 18:16:01 +0600

    add new project link and image

--- src/component/Portfoli.js
+++ src/component/Portfoli.js
@@ -3,7 +3,7 @@ import {Grid} from '@mui/material'
 import CardUi from './Card';
 import '../styles/portfolio.css'
 import CardImg from '../Assist/weather-app.png'
-import CardImg1 from '../Assist/default.jpg'
+import CardImg1 from '../Assist/html-css-bootstrap.png'
 import CardImg2 from '../Assist/portfolio.png'
 import CardImg3 from '../Assist/default.jpg'
 import CardImg4 from '../Assist/task-m.png'
@@ -19,23 +19,42 @@ function Portfolio() {
             <Grid item xs={12} sm={6} md={4}>
             <CardUi 
               Img={CardImg}
-              Title={'WatherApp'}
+              Title={'WatherApp It build with <ReactJs,Axios,Open Weather API,Html,Css />'}
               Link={'https://tinney-weather-app.vercel.app/'}
             />
             </Grid>
             <Grid item xs={12} sm={6} md={4}>
             <CardUi 
-            Img={CardImg4} 
-            Title={'Task_M'}
-            Link={'https://task-m.vercel.app/'}
+              Img={CardImg4} 
+              Title={'Task_M It build with <ReactJs,React D&D,React Hooks,Html,Css,Css(Flex Box) />'}
+              Link={'https://task-m.vercel.app/'}
             />
             </Grid>
             <Grid item xs={12} sm={6} md={4}>
-            <CardUi Img={CardImg2} Title={'PortFolioApp'} />
+            <CardUi Img={CardImg2} Title={'Portfolio App It build with <ReactJs,Hrml,Css,Mui />'} />
             </Grid>
             <Grid item xs={12} sm={6} md={4}>
-            <CardUi Img={CardImg3} Title={'SnackGame (Comming Soon)'} />
+            <CardUi 
+              Img={CardImg1} 
+              Title={'Html,Css,Bootstrap (static)'}
+              Link={'https://html-css-bootstrap-eight.vercel.app/'}
+            />
             </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+            <CardUi 
+              Img={CardImg3} 
+              Title={'Coming soon'}
+              // Link={'https://html-css-bootstrap-eight.vercel.app/'}
+            />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+            <CardUi 
+              Img={CardImg3} 
+              Title={'Comming soon'}
+              // Link={'https://html-css-bootstrap-eight.vercel.app/'}
+            />
+            </Grid>
+            
             <div className='bottom-baseLine'></div>
           </Grid>
           
